5e75c71756543f9c4c040a99c4599b8a7faf20b7
Karl Ward <karl@ghostghost.net>
2013-11-04 09:06:38 -0500

    removed socket.io from server.js --KW

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-var io = require('socket.io').listen(server);
 
 // configure Express
 app.use(express.bodyParser());
